b78bdbdd23d8fe4ee0f33a36fe5739bf5b8540a5
Christian Zufferey <christian@zufferey.com>
2025-12-08 15:03:02 +0100

    new: ajouté un nouveau dossier sur le synogmère pour le nouveau pbs

--- rsync_script.sh
+++ rsync_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour exécuter un rsync entre la source et le remote via le port 2222
-#zf251107.1633, zf251107.1653
+#zf251107.1633, zf251208.1304
 
 echo -e "\nExécute le rsync...\n"
 
@@ -8,7 +8,17 @@ cd ~/dev/z-rsync-remote-pbs
 
 date +"%F %T"
 
-time rsync -n --delete -r -v -t --progress --stats --modify-window=1  -e "ssh -p 2222" /mnt/datastore/ext-1/ master@localhost:/volume2/BIGDATA/data/backup_pbs_crissier_ext-1_251107.1648/
+# Pour le synogmère
+#time rsync -n --delete -r -v -t --progress --stats --modify-window=1  -e "ssh -p 2222" /mnt/datastore/ext-1/ master@localhost:/volume2/BIGDATA/data/backup_pbs_crissier_ext-1_251208.1313/
+time rsync     --delete -r -v -t --progress --stats --modify-window=1  -e "ssh -p 2222" /mnt/datastore/ext-1/ master@localhost:/volume2/BIGDATA/data/backup_pbs_crissier_ext-1_251208.1313/
+
+#time rsync    --delete -r -v -t --progress --stats --modify-window=1  -e "ssh -p 2222" /mnt/datastore/ext-1/ master@localhost:/volume2/BIGDATA/data/backup_pbs_crissier_ext-1_251107.1648/
+#time rsync --delete -r -v -t --progress --stats --modify-window=1  -e ssh /mnt/datastore/ext-1/ master@100.79.152.38:/volume2/BIGDATA/data/backup_pbs_crissier_ext-1_251107.1648/
+
+# Pour le nuc-tst1
+#time rsync -n --delete -r -v -t --progress --stats --modify-window=1  -e "ssh -p 22" /mnt/datastore/ext-1/ root@192.168.0.39:/mnt/pve/ext-4/data/
+#time rsync --delete -r -v -t --progress --stats --modify-window=1  -e "ssh -p 22" /mnt/datastore/ext-1/ root@192.168.0.39:/mnt/pve/ext-4/data/
+
 
 echo ""
 date +"%F %T"
